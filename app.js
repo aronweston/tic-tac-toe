1430e0deb96050b00bbd17410740bad1fe457565
aronweston <aron@firststepdigital.com.au>
2021-01-05 13:38:14 +1100

    #2: cleaner version; passing in choices arrays to checkWinner method that will filter down through if checks which will eventually set an empty variable as the winner. Possibly consider using the .filter() method but we'll see how this goes

--- app.js
+++ app.js
@@ -32,72 +32,43 @@ const game = {
         } else {
             $(e.target).html("<img id='entry' src='img/nought.svg'></>");
             p2.choices.push(e.target.id);
-            p2.turn = false;
+            p1.turn = true;
         }
+    
        game.checkWinner(p1.choices, p2.choices)
     },
     checkWinner: function (p1, p2) {
 
-        console.log(p1);
-        console.log(p2);
+        //Horizontal win conditions are left word of the id: top, middle, bottom
+        //Vertical: right word of the id - left, center, right
+        //Diagonal
+        //Tie = the combined length of both arrays are equal to 9
+        
+        // //loop through or check if the array contains the winning   
+        // if()
 
+        let winner;
+        
+        if (p1.length >= 3 || p2.length >= 3) {
+            console.log("Player 1", p1, "Player 2", p2);
 
-
-
+        } 
+    
+        //TIE CONDITION
+        // const tie = p1.length + p2.length;
+        // if (tie >= 9) {
+        //     alert("it's a tie");
+        //     window.location.reload();
+        // }
 
     }
 }
 
 document.querySelector('#board').addEventListener('click', game.playerTurn);
 
-
 // document.querySelector('#play-again').addEventListener('click', function () {
 //     window.location.reload();
 // });
 
 
-  playerTurn: function (e) {
-        const p1 = game.playerOne;
-        const p2 = game.playerTwo;
 
-        if (p1.turn === true) {
-            $(e.target).html("<img id='entry' src='img/cross.svg'></>");
-            p1.choices.push(e.target.id);
-            p1.turn = false;
-        } else {
-            $(e.target).html("<img id='entry' src='img/nought.svg'></>");
-            p2.choices.push(e.target.id);
-            p2.turn = false;
-        }
-       game.checkWinner(p1.choices, p2.choices)
-    },
-    checkWinner: function (p1, p2) {
-
-        console.log(p1);
-        console.log(p2);
-
-
-
-
-
-    }
-
-
-
-
-
-
-
-//  loop: function () {
-//         const squares = document.querySelectorAll('.square');
-//         const newSquares = Array.from(squares);
-//         // console.log(newSquares);
-
-//         newSquares.forEach(square => { 
-
-//             // if (square.children[0].id )
-
-//             console.log(square.children.id);
-//         })
-
-//     },
