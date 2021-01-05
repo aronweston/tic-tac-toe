37d18efb0deffaea5a31faec3e6b5a9765319fcf
aronweston <aron@firststepdigital.com.au>
2021-01-05 15:49:32 +1100

    #3: working version of player one horizontal and vertical win checks by 'looping' the hoz and vert player 1 arrays with .every() , no diagonal or player two interegration yet. Should turn check winner into a ultility function to output the winner and where they won for both players and for diag

--- app.js
+++ app.js
@@ -25,35 +25,84 @@ const game = {
         const p1 = game.playerOne;
         const p2 = game.playerTwo;
 
+
+        //TODO: disable click after 
         if (p1.turn === true) {
-            $(e.target).html("<img id='entry' src='img/cross.svg'></>");
+            $(e.target).html("<img src='img/cross.svg'></>");
             p1.choices.push(e.target.id);
             p1.turn = false;
         } else {
-            $(e.target).html("<img id='entry' src='img/nought.svg'></>");
+            $(e.target).html("<img src='img/nought.svg'></>");
             p2.choices.push(e.target.id);
             p1.turn = true;
         }
-    
-       game.checkWinner(p1.choices, p2.choices)
+
+        game.checkWinner(p1.choices, p2.choices)
     },
     checkWinner: function (p1, p2) {
-
+        let winner;
         //Horizontal win conditions are left word of the id: top, middle, bottom
         //Vertical: right word of the id - left, center, right
         //Diagonal
         //Tie = the combined length of both arrays are equal to 9
-        
         // //loop through or check if the array contains the winning   
-        // if()
 
-        let winner;
+        const hoz = ['top', 'middle', 'bottom'];
+        const vert = ['left', 'center', 'right'];
         
-        if (p1.length >= 3 || p2.length >= 3) {
-            console.log("Player 1", p1, "Player 2", p2);
 
-        } 
-    
+        //If the length is less then 3 then we don't care
+        if (p1.length >= 3 || p2.length >= 3) {
+
+            // console.log(p1);
+
+            let p1Hoz = [];
+            let p1Vert = [];
+
+            //Create a two new arrays, the hoz and vertical values of player one
+            p1.forEach(item => {
+                let dashIndex = item.indexOf('-')
+                let hoz = item.slice(0, dashIndex);
+                let vert = item.slice(dashIndex + 1, item.length);
+                p1Hoz.push(hoz);
+                p1Vert.push(vert);
+            })
+
+            console.log(p1Hoz);
+            // console.log(hoz[0]);
+
+           
+            let hozWinCheck = p1Hoz.every(function (value) {
+            if (value === hoz[0]) {
+                console.log('Player 1 wins Top Row');
+                    return true;
+                } else if (value === hoz[1]) {
+                    console.log('Player 1 wins Middle Row');
+                    return true;
+                } else if (value === hoz[2]) {
+                    console.log('Player 1 wins Bottom Row');
+                    return true;
+                }
+            })
+
+            let vertWinCheck = p1Vert.every(function (value) {
+            if (value === hoz[0]) {
+                console.log('Player 1 wins Col Row');
+                    return true;
+                } else if (value === hoz[1]) {
+                    console.log('Player 1 wins Center Col');
+                    return true;
+                } else if (value === hoz[2]) {
+                    console.log('Player 1 wins <Col></Col> Row');
+                    return true;
+                }
+            })
+
+
+            console.log(hozWinCheck);
+            console.log(vertWinCheck);
+        }
+
         //TIE CONDITION
         // const tie = p1.length + p2.length;
         // if (tie >= 9) {
@@ -69,6 +118,3 @@ document.querySelector('#board').addEventListener('click', game.playerTurn);
 // document.querySelector('#play-again').addEventListener('click', function () {
 //     window.location.reload();
 // });
-
-
-
