d9bef6e1f1b359e254c867f681cc71699560972d
aronweston <aron@firststepdigital.com.au>
2021-01-07 15:30:50 +1100

    #5: Win conditions working but tie condition not fully ironed out, alerting to the user which side won in a single method. TODO: Tell them where they won, fix up tie condition

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function () {
 
     //Build the scoreboard
--- js/game.js
+++ js/game.js
@@ -50,22 +50,30 @@ const game = {
             }
         })
     },
-    alertWinner: function (msg, winner, loser) {
+    alertWinner: function (winner, loser, winArr) {
+
         //Let them know who won and where
-        alert(msg);
+        alert(`${winner.name} wins! Bad luck, ${loser.name}`);
 
         //Add to scores
         winner.results.won++;
         loser.results.lost++;
 
+        console.log(winArr);
+
         //Update the score
         $('#tp-p1 span').text(`WON: ${game.playerOne.results.won}`);
         $('#tp-p2 span').text(`WON: ${game.playerTwo.results.won}`);
+        //Clear the board after 1 second
+        setTimeout(() => {
+            game.clearBoard();
+        }, 1000);
     },
     checkWinner: function (p1, p2) {
 
         const playerOne = p1.choices.sort();
         const playerTwo = p2.choices.sort();
+        const tie = playerOne.length + playerTwo.length;
 
         const win = [
             [1, 2, 3],
@@ -77,52 +85,38 @@ const game = {
             [1, 5, 9],
             [3, 5, 7]
         ]
+        
 
-        //ES5+
-        //1. Loop the winning arrays to test each array each time
-        //2. Now I have the winning arrays, I want to loop them and get their values for checking
-        //3. Check if the values of each of the 
-        for (i = 0; i < win.length; i++) {
-            const winners = win[i];
-            for (j = 0; j < winners.length; j++) {
-                const winValues = winners[j];
-                if (winners.length === playerOne.length) {
-                    let x = playerOne.every(function (value) {
-                        if (value === winValues) {
-                            return 'winner'
-                        }
-                    })
+        const check = (win, arr) => {
+            if (win.length === arr.length) {
+                if (win.every((value, index) => value === arr[index])) {
+                    return true;
                 }
             }
         }
 
+        // (msg, winner, loser, winArr)
 
-        //ES6+
-        win.forEach(winner => {
-            if (Array.isArray(winner) && Array.isArray(playerOne)) {
-                if (winner.length === playerOne.length) {
-                    let check = winner.every(function (value, index) {
-                        console.log("Winner value", value);
-                        console.log("Player one", playerOne);
-                        console.log("Player one index", playerOne[index]);
-                        console.log("Player one index", playerOne[index]);
-                        console.log("Player one index", playerOne[index]);
-                        // console.log(value);
-                        // console.log(winner);
-                        // console.log(value === playerOne[index]);
-
-                        if (value === playerOne[index]) {
-                            console.log(playerOne[index]);
-                            return true;
-
-                        }
-                    });
-                    console.log(check);
+        for (i = 0; i < win.length; i++) {
+            let res;
+            if (check(win[i], playerOne)) {
+                res = "player 1";
+                game.alertWinner(p1, p2, win[i]);
+            } else if (check(win[i], playerTwo)) {
+                res = "player 2";
+                game.alertWinner(p2, p1, win[i]);
+            } else {
+                if (tie > 9) {
+                    if (res !== "player 1" || res !== "player 2") {
+                        alert('tie');
+                        setTimeout(() => {
+                            game.clearBoard();
+                        }, 1000);
+                        break;
+                    }
                 }
             }
-        });
-
-
+        }
     }
 }
 
@@ -130,17 +124,7 @@ const p1 = game.playerOne;
 const p2 = game.playerTwo;
 
 
-
-
-// ,{"hozMiddle": [4, 5, 6]
-//     "hozBottom": [7, 8, 9],
-//     "leftVert": [1, 4, 7],
-//     "centerVert": [2, 5, 8],
-//     "rightVert": [3, 6, 9],
-//     "leftDiag": [1, 5, 9],
-//     "rightDiag": [3, 5, 7]
-// }
-
-// //Alerts to the dom
-// game.alertWinner('Player 1 wins top row', p1, p2);
-// game.clearBoard();
+//ES6+
+//1. Loop through the winner arrays and save the winning arrays to a winner variable
+//2. Check if both are arrays and also if they have the same length
+//3. Loop through each of the winner arrays and check to see if each of the values of the winner array match the value of the playerOne array, at the index of the winner array value. So if win[0,1,2] = playerOne[0,1,2], else, check the next array until there's no more 
