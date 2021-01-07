1486b888c88579a2fc2e1b9534a91243ccb8be72
aronweston <aron@firststepdigital.com.au>
2021-01-08 00:43:38 +1100

    #7: Final working version at the end of the current required goals

--- js/app.js
+++ js/app.js
@@ -1,29 +1,6 @@
-
 $(document).ready(function () {
 
-
-    //Build the scoreboard
-    const buildScoreBoard = () => {
-        //Remove the hidden class
-        $("#player-cards").removeClass('hide');
-
-        //Set player 1 output
-        $('#tp-p1').html(`<div>${p1.icon} ${p1.name}<span class="secondary-content">WIN: ${p1.results.won}</span></div>`);
-        //Set player 2 output
-        $('#tp-p2').html(`<div>${p2.icon} ${p2.name}<span class="secondary-content">WIN: ${p2.results.won}</span></div>`);
-
-        $.fn.pagepiling.moveSectionDown("game-board");
-    }
-
-    //pagepiling.js for scroll effects
-    $('#pagepiling').pagepiling({
-        menu: null,
-        navigation: false,
-        sectionsColor: ['white', 'white', 'white', 'white'],
-        anchors: ['intro', 'player-choice', 'one', 'two', 'game-board'],
-    });
-
-    //Set names and start 
+    //On submit of the player two section, each icon and name are set to their respective objects. After some small form validation, the score board is generated in JS. 
     $("form#two-input").on("submit", function (e) {
         //Set the name values
         game.playerOne.name = $('#two-players-one').val()
@@ -41,9 +18,24 @@ $(document).ready(function () {
             //if all good, build the score board and set the icons
             game.playerOne.icon = player1Icon;
             game.playerTwo.icon = player2Icon;
-            buildScoreBoard();
+            game.buildScoreBoard();
         }
         e.preventDefault();
+    });
+
+    //If there are names on the board, then the players can play the game. 
+    $('#board').on('click', function (e) {
+        if (game.playerOne.name.length > 0 && game.playerTwo.name.length > 0) {
+            game.playerTurn(e);
+        }
+    });
+
+    // pagepiling.js for scroll effects. This jQuery library creates the scrolling effects for each of the pages. Documentation available: https://alvarotrigo.com/pagePiling/
+    $('#pagepiling').pagepiling({
+        menu: null,
+        navigation: false,
+        sectionsColor: ['white', 'white', 'white', 'white'],
+        anchors: ['player-choice', 'two', 'game-board'],
     });
 
     //Clear board on button click
@@ -51,12 +43,4 @@ $(document).ready(function () {
         game.clearBoard()
     })
 
-    //If we have names, proceed to the thing 
-    $('#board').on('click', function (e) {
-        if (game.playerOne.name.length > 0 && game.playerTwo.name.length > 0) {
-            game.playerTurn(e);
-        }
-    });
-
 })
-
--- js/game.js
+++ js/game.js
@@ -1,6 +1,6 @@
 const game = {
     playerOne: {
-        name: 'Player 1',
+        name: 'Frank',
         icon: 'X',
         turn: true,
         choices: [],
@@ -9,9 +9,8 @@ const game = {
             lost: 0
         }
     },
-    
     playerTwo: {
-        name: 'Player 2',
+        name: 'Steve',
         icon: 'O',
         turn: false,
         choices: [],
@@ -21,13 +20,17 @@ const game = {
         }
     },
     playerTurn: function (e) {
+        //Bring in the 
         const p1 = game.playerOne;
         const p2 = game.playerTwo;
-        //TODO: disable click so they don't chuck in trash into the array, no more entries after the single click
+        
+        //Once the game has started and the names are entered. Player 1 defaults to X and its their turn first. As the event object is passed in we can both access the target ID and push to each players choices array. And, we can output their chosen icon to the same target in the DOM. We then add the active classes to show who's turn it is. 
+
         if (p1.turn === true) {
-            $(e.target).html(`<span>${p1.icon}</span>`);
             p1.choices.push(Number(e.target.id));
+            $(e.target).html(`<span>${p1.icon}</span>`);
             p1.turn = false;
+            //Show in the tally box who's turn it is 
             $('#tp-p1').removeClass('active');
             $('#tp-p2').addClass('active');
         } else {
@@ -39,49 +42,13 @@ const game = {
         }
         game.checkWinner(p1, p2)
     },
-    clearBoard: function () {
-        //Clear P1 & P2 choices to reset the game
-        game.playerOne.choices = [];
-        game.playerTwo.choices = [];
-        game.playerOne.turn = true;
-        //Get board and remove everything in each square
-        game.loopBoard((square) => {
-            if (square.children.length > 0) {
-                square.childNodes[0].remove();
-            }
-        });
-    },
-    loopBoard: function (callBack) {
-        const board = Array.from(document.getElementById('board').children);
-        board.forEach(square => {
-            callBack(square);
-        })
-    },
-    colorDOM: function (a, b, c) {
-        $(`div#${a}`).addClass('green');
-        $(`div#${b}`).addClass('green');
-        $(`div#${c}`).addClass('green');
-        //Timeout 
-        setTimeout(() => {
-            $(`div#${a}`).removeClass('green');
-            $(`div#${b}`).removeClass('green');
-            $(`div#${c}`).removeClass('green');
-        }, 2000);
-    },
-    colorTie: function (t) {
-        if (t) {
-            $('div.square').addClass('red');
-            setTimeout(() => {
-                $('div.square').removeClass('red');
-            }, 2000);
-        }
-    },
     checkWinner: function (p1, p2) {
-
+        //Get both players choices and the tie condition of when the total length is equal to 9;
         const playerOne = p1.choices.sort();
         const playerTwo = p2.choices.sort();
         const tie = playerOne.length + playerTwo.length;
-        
+
+        //Win conditions based on the positions of the board. Easier saving this as an array of arrays due to more options for loops. 
         const win = [
             [1, 2, 3],
             [4, 5, 6],
@@ -93,7 +60,7 @@ const game = {
             [3, 5, 7]
         ]
 
-        //Checker
+        //check() - takes two arguments, the array it is called on and the winning array destructured. Part of the ES6 syntax, we can now access the index values of the array passed into this function. The second argument is the array the function will check, player one or two. If check = true, someone has won.    
         const check = ([a, b, c], arr) => {
             if (arr.includes(a) && arr.includes(b) && arr.includes(c)) {
                 return true;
@@ -102,63 +69,135 @@ const game = {
             }
         }
 
-        //Checker with working win and tie check conditions
+        // Condition Loop - this loops through the win array and returns each winning array, then the check function is called to determine which player is the winner. If none and the squares are occupied, it's a tie.
         for (i = 0; i < win.length; i++) {
             const oneWin = check(win[i], playerOne)
             const twoWin = check(win[i], playerTwo)
-            
+
             if (oneWin) {
                 game.alertWinner(p1, p2, win[i]);
             }
-            
+
             if (twoWin) {
                 game.alertWinner(p2, p1, win[i]);
             }
-            
-            if (tie === 9 && !twoWin && !oneWin ) {
-                console.log('Tie');
-                game.colorTie(true);
+
+            if (tie === 9 && !twoWin && !oneWin) {
+                game.showTie(true);
                 setTimeout(() => {
                     game.clearBoard();
                 }, 2000);
                 break;
             }
         };
-
-
     },
     alertWinner: function (winner, loser, winArr) {
+        //Let them know who won and where by removing the hide class from the alert bar, adding in the words of encouragement, then hiding it again in 2 seconds.
+        $('#alert').removeClass('hide');
+        $('#alert p').text(`${winner.name} wins! Bad luck, ${loser.name}`);
+        setTimeout(() => {
+            $('#alert').addClass('hide');
+        }, 2000);
 
-        //Let them know who won and where
-        console.log(`${winner.name} wins! Bad luck, ${loser.name}`);
-
-        //Add to scores
+        //Add one to players scores every time they win or lose. This method takes in the winner and loser objects. 
         winner.results.won++;
         loser.results.lost++;
 
-        //Destructure the winning array and get the values 
-        const [first, second, third] = winArr;
-        //Loop the board and output 
-        game.loopBoard(function (square) {
-            //If the id of the square matches the winning array number. Could probably make this more explicit by not turning the squares red on a tie. 
-            if (Number(square.id) === first && Number(square.id) === first && Number(square.id) === first) {
-                game.colorDOM(first, second, third, false);
-            } else {
-                game.colorTie(true);
-            }
-        })
-
-        //Update the score
+        //Output above to DOM
         $('#tp-p1 span').text(`WON: ${game.playerOne.results.won}`);
         $('#tp-p2 span').text(`WON: ${game.playerTwo.results.won}`);
 
-        //Clear the board after 1 second
+        //Let the user know where they won.
+
+        //1. Destructure the winning array and get the values. This will give us the 1-3 integers that we can then match against the ID's of the board 
+        const [first, second, third] = winArr;
+        //2. run colorDOM() that takes in each of the elements within above winArr array, loops the board and adds a green class to the square that had the winning combo.
+        game.showWin(first, second, third);
+        game.clearBoard();
+    },
+    //Build the scoreboard
+    buildScoreBoard: function () {
+        //Remove the hidden class
+        $("#player-cards").removeClass('hide');
+        //Set player 1 output
+        $('#tp-p1').html(`<div>${p1.icon} ${p1.name}<span class="secondary-content">WIN: ${p1.results.won}</span></div>`);
+        //Set player 2 output
+        $('#tp-p2').html(`<div>${p2.icon} ${p2.name}<span class="secondary-content">WIN: ${p2.results.won}</span></div>`);
+        $.fn.pagepiling.moveSectionDown("game-board");
+    },
+    clearBoard: function () {
+        //Clear the player one and player two selection arrays to restart the game
+        game.playerOne.choices = [];
+        game.playerTwo.choices = [];
+
+        //Give player one their starting turn back
+        game.playerOne.turn = true;
+
         setTimeout(() => {
-            game.clearBoard();
+            //Get board and remove everything in each square
+            game.loopBoard((square) => {
+            if (square.children.length > 0) {
+                square.childNodes[0].remove();
+            }
+        });
         }, 1000);
+    },
+    // A utility method used throughout the project to prevent repeating loops everywhere and to provide a way of getting all of the squares on the board for different conditionals. This is achieved through the callBack function that will execute within a loop on the board array.
+    loopBoard: function (callBack) {
+        const board = Array.from(document.getElementById('board').children);
+        board.forEach(square => {
+            callBack(square);
+        })
+    },
+    showWin: function (a, b, c) {
+        //Add the winning array ID's to target the DOM elements and add a green class to them when they have won.
+        $(`div#${a}`).addClass('green');
+        $(`div#${b}`).addClass('green');
+        $(`div#${c}`).addClass('green');
+        setTimeout(() => {
+            $(`div#${a}`).removeClass('green');
+            $(`div#${b}`).removeClass('green');
+            $(`div#${c}`).removeClass('green');
+        }, 1000);
+    },
+    showTie: function (t) {
+        if (t) {
+            $('#alert').removeClass('hide');
+            $('div.square').addClass('red');
+            $('#alert p').text('A tie. Lame, you both suck');
+            setTimeout(() => {
+                $('#alert').addClass('hide');
+                $('div.square').removeClass('red');
+            }, 1000);
+        }
     }
 }
 
-
+//Declare the object variables to feed to app.js
 const p1 = game.playerOne;
 const p2 = game.playerTwo;
+
+// Stretch Goals
+
+// getPlayer1: function (player) {
+//     let player;
+//     if (!localStorage.getItem('player1')) {
+//         player = [];
+//     } else {
+//         player = JSON.parse(localStorage.getItem('player1'));
+//     }
+//     return player;
+// },
+// getPlayer2: function (player) {
+//     let player;
+//     if (!localStorage.getItem('player2')) {
+//         player = [];
+//     } else {
+//         player = JSON.parse(localStorage.getItem('player2'));
+//     }
+//     return player;
+// },
+// addStorage: function (obj) {
+//     const player = game.getPlayers
+
+// }
